74ad20a0d80ae2038301b4846cfe8bdde124c2dc
Vignesh <vvignesh10399@gmail.com>
2025-04-04 10:41:04 +0530

    123

---
+++ src/main/java/com/jpahibernate/example/student_management_db/repository/StudentRepository.java
@@ -0,0 +1,4 @@
+package com.jpahibernate.example.student_management_db.repository;
+
+public interface StudentRepository {
+}
---
+++ src/main/java/com/jpahibernate/example/student_management_db/service/StudentService.java
@@ -0,0 +1,4 @@
+package com.jpahibernate.example.student_management_db.service;
+
+public class StudentService {
+}
